steipete/remindctl

feat: add tags (hashtag) support
Add support for reading, writing, and filtering Apple Reminders tags.

Tags in Apple Reminders are stored as #hashtags in the reminder title
text. This PR adds first-class CLI support for working with them.

New features:
- `remindctl tags` - list all unique tags with counts
- `remindctl tags <name>` - list reminders matching a tag
- `remindctl add --tag <name>` - create reminders with tags
- `remindctl edit --tag <name>` - add tags to existing reminders
- `remindctl edit --remove-tag <name>` - remove specific tags
- `remindctl edit --clear-tags` - remove all tags
- `remindctl show --tag <name>` - filter reminders by tag
- Tags are included in JSON output (tags array + titleWithoutTags)

Tags can be specified as repeatable flags (--tag a --tag b) or
comma-separated (--tag a,b). Tag matching is case-insensitive.

Closes #18

Sources/remindctl/Commands/AddCommand.swift
@@ -19,6 +19,12 @@ enum AddCommand {
             .make(label: "due", names: [.short("d"), .long("due")], help: "Due date", parsing: .singleValue),
             .make(label: "notes", names: [.short("n"), .long("notes")], help: "Notes", parsing: .singleValue),
             .make(
+              label: "tag",
+              names: [.long("tag")],
+              help: "Tag name (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
+            .make(
               label: "priority",
               names: [.short("p"), .long("priority")],
               help: "none|low|medium|high",
@@ -31,6 +37,8 @@ enum AddCommand {
         "remindctl add \"Buy milk\"",
         "remindctl add --title \"Call mom\" --list Personal --due tomorrow",
         "remindctl add \"Review docs\" --priority high",
+        "remindctl add \"Buy milk\" --tag shopping --tag urgent",
+        "remindctl add \"Buy milk\" --tag shopping,urgent",
       ]
     ) { values, runtime in
       let titleOption = values.option("title")
@@ -56,9 +64,14 @@ enum AddCommand {
       let notes = values.option("notes")
       let dueValue = values.option("due")
       let priorityValue = values.option("priority")
+      let tagValues = values.optionValues("tag")
 
       let dueDate = try dueValue.map(CommandHelpers.parseDueDate)
       let priority = try priorityValue.map(CommandHelpers.parsePriority) ?? .none
+      let tags = try CommandHelpers.parseTags(tagValues)
+      let parsedTitle = CommandHelpers.parseTitleTags(title)
+      let mergedTags = CommandHelpers.mergeTags(existing: parsedTitle.tags, add: tags, remove: [], clear: false)
+      let titleWithTags = CommandHelpers.composeTitle(baseTitle: parsedTitle.baseTitle, tags: mergedTags)
 
       let store = RemindersStore()
       try await store.requestAccess()
@@ -73,7 +86,7 @@ enum AddCommand {
         throw RemindCoreError.operationFailed("No default list found. Specify --list.")
       }
 
-      let draft = ReminderDraft(title: title, notes: notes, dueDate: dueDate, priority: priority)
+      let draft = ReminderDraft(title: titleWithTags, notes: notes, dueDate: dueDate, priority: priority)
       let reminder = try await store.createReminder(draft, listName: targetList)
       OutputRenderer.printReminder(reminder, format: runtime.outputFormat)
     }

Sources/remindctl/Commands/EditCommand.swift
@@ -19,6 +19,18 @@ enum EditCommand {
             .make(label: "due", names: [.short("d"), .long("due")], help: "Set due date", parsing: .singleValue),
             .make(label: "notes", names: [.short("n"), .long("notes")], help: "Set notes", parsing: .singleValue),
             .make(
+              label: "tag",
+              names: [.long("tag")],
+              help: "Add tag (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
+            .make(
+              label: "removeTag",
+              names: [.long("remove-tag")],
+              help: "Remove tag (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
+            .make(
               label: "priority",
               names: [.short("p"), .long("priority")],
               help: "none|low|medium|high",
@@ -27,6 +39,7 @@ enum EditCommand {
           ],
           flags: [
             .make(label: "clearDue", names: [.long("clear-due")], help: "Clear due date"),
+            .make(label: "clearTags", names: [.long("clear-tags")], help: "Remove all tags"),
             .make(label: "complete", names: [.long("complete")], help: "Mark completed"),
             .make(label: "incomplete", names: [.long("incomplete")], help: "Mark incomplete"),
           ]
@@ -37,6 +50,8 @@ enum EditCommand {
         "remindctl edit 4A83 --due tomorrow",
         "remindctl edit 2 --priority high --notes \"Call before noon\"",
         "remindctl edit 3 --clear-due",
+        "remindctl edit 1 --tag urgent --remove-tag someday",
+        "remindctl edit 1 --clear-tags",
       ]
     ) { values, runtime in
       guard let input = values.argument(0) else {
@@ -51,9 +66,25 @@ enum EditCommand {
         throw RemindCoreError.reminderNotFound(input)
       }
 
-      let title = values.option("title")
+      var title = values.option("title")
       let listName = values.option("list")
       let notes = values.option("notes")
+      let addTags = try CommandHelpers.parseTags(values.optionValues("tag"))
+      let removeTags = try CommandHelpers.parseTags(values.optionValues("removeTag"))
+      let clearTags = values.flag("clearTags")
+      let hasTagChange = !addTags.isEmpty || !removeTags.isEmpty || clearTags
+
+      if hasTagChange {
+        let sourceTitle = title ?? reminder.title
+        let parsed = CommandHelpers.parseTitleTags(sourceTitle)
+        let merged = CommandHelpers.mergeTags(
+          existing: parsed.tags,
+          add: addTags,
+          remove: removeTags,
+          clear: clearTags
+        )
+        title = CommandHelpers.composeTitle(baseTitle: parsed.baseTitle, tags: merged)
+      }
 
       var dueUpdate: Date??
       if let dueValue = values.option("due") {
@@ -78,7 +109,9 @@ enum EditCommand {
       }
       let isCompleted: Bool? = completeFlag ? true : (incompleteFlag ? false : nil)
 
-      if title == nil && listName == nil && notes == nil && dueUpdate == nil && priority == nil && isCompleted == nil {
+      if title == nil && listName == nil && notes == nil && dueUpdate == nil && priority == nil
+        && isCompleted == nil && !hasTagChange
+      {
         throw RemindCoreError.operationFailed("No changes specified")
       }
 

Sources/remindctl/Commands/ShowCommand.swift
@@ -23,7 +23,13 @@ enum ShowCommand {
               names: [.short("l"), .long("list")],
               help: "Limit to a specific list",
               parsing: .singleValue
-            )
+            ),
+            .make(
+              label: "tag",
+              names: [.long("tag")],
+              help: "Filter by tag (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
           ]
         )
       ),
@@ -33,10 +39,12 @@ enum ShowCommand {
         "remindctl show overdue",
         "remindctl show 2026-01-04",
         "remindctl show --list Work",
+        "remindctl show --tag shopping",
       ]
     ) { values, runtime in
       let listName = values.option("list")
       let filterToken = values.argument(0)
+      let tagFilters = try CommandHelpers.parseTags(values.optionValues("tag")).map { $0.lowercased() }
 
       let filter: ReminderFilter
       if let token = filterToken {
@@ -51,7 +59,17 @@ enum ShowCommand {
       let store = RemindersStore()
       try await store.requestAccess()
       let reminders = try await store.reminders(in: listName)
-      let filtered = ReminderFiltering.apply(reminders, filter: filter)
+      let filteredByDate = ReminderFiltering.apply(reminders, filter: filter)
+      let filtered: [ReminderItem]
+      if tagFilters.isEmpty {
+        filtered = filteredByDate
+      } else {
+        let tagFilterSet = Set(tagFilters)
+        filtered = filteredByDate.filter { reminder in
+          let reminderTags = Set(reminder.tags.map { $0.lowercased() })
+          return !tagFilterSet.isDisjoint(with: reminderTags)
+        }
+      }
       OutputRenderer.printReminders(filtered, format: runtime.outputFormat)
     }
   }

henrisaksi/reminders

Sources/reminders/Commands/AddCommand.swift
@@ -19,6 +19,12 @@ enum AddCommand {
             .make(label: "due", names: [.short("d"), .long("due")], help: "Due date", parsing: .singleValue),
             .make(label: "notes", names: [.short("n"), .long("notes")], help: "Notes", parsing: .singleValue),
             .make(
+              label: "tag",
+              names: [.long("tag")],
+              help: "Tag name (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
+            .make(
               label: "priority",
               names: [.short("p"), .long("priority")],
               help: "none|low|medium|high",
@@ -31,6 +37,8 @@ enum AddCommand {
         "reminders add \"Buy milk\"",
         "reminders add --title \"Call mom\" --list Personal --due tomorrow",
         "reminders add \"Review docs\" --priority high",
+        "reminders add \"Buy milk\" --tag shopping --tag urgent",
+        "reminders add \"Buy milk\" --tag shopping,urgent",
       ]
     ) { values, runtime in
       let titleOption = values.option("title")
@@ -56,9 +64,14 @@ enum AddCommand {
       let notes = values.option("notes")
       let dueValue = values.option("due")
       let priorityValue = values.option("priority")
+      let tagValues = values.optionValues("tag")
 
       let dueDate = try dueValue.map(CommandHelpers.parseDueDate)
       let priority = try priorityValue.map(CommandHelpers.parsePriority) ?? .none
+      let tags = try CommandHelpers.parseTags(tagValues)
+      let parsedTitle = CommandHelpers.parseTitleTags(title)
+      let mergedTags = CommandHelpers.mergeTags(existing: parsedTitle.tags, add: tags, remove: [], clear: false)
+      let titleWithTags = CommandHelpers.composeTitle(baseTitle: parsedTitle.baseTitle, tags: mergedTags)
 
       let store = RemindersStore()
       try await store.requestAccess()
@@ -73,7 +86,7 @@ enum AddCommand {
         throw RemindCoreError.operationFailed("No default list found. Specify --list.")
       }
 
-      let draft = ReminderDraft(title: title, notes: notes, dueDate: dueDate, priority: priority)
+      let draft = ReminderDraft(title: titleWithTags, notes: notes, dueDate: dueDate, priority: priority)
       let reminder = try await store.createReminder(draft, listName: targetList)
       OutputRenderer.printReminder(reminder, format: runtime.outputFormat)
     }

Sources/reminders/Commands/EditCommand.swift
@@ -19,6 +19,18 @@ enum EditCommand {
             .make(label: "due", names: [.short("d"), .long("due")], help: "Set due date", parsing: .singleValue),
             .make(label: "notes", names: [.short("n"), .long("notes")], help: "Set notes", parsing: .singleValue),
             .make(
+              label: "tag",
+              names: [.long("tag")],
+              help: "Add tag (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
+            .make(
+              label: "removeTag",
+              names: [.long("remove-tag")],
+              help: "Remove tag (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
+            .make(
               label: "priority",
               names: [.short("p"), .long("priority")],
               help: "none|low|medium|high",
@@ -27,6 +39,7 @@ enum EditCommand {
           ],
           flags: [
             .make(label: "clearDue", names: [.long("clear-due")], help: "Clear due date"),
+            .make(label: "clearTags", names: [.long("clear-tags")], help: "Remove all tags"),
             .make(label: "complete", names: [.long("complete")], help: "Mark completed"),
             .make(label: "incomplete", names: [.long("incomplete")], help: "Mark incomplete"),
           ]
@@ -37,6 +50,8 @@ enum EditCommand {
         "reminders edit 4A83 --due tomorrow",
         "reminders edit 2 --priority high --notes \"Call before noon\"",
         "reminders edit 3 --clear-due",
+        "reminders edit 1 --tag urgent --remove-tag someday",
+        "reminders edit 1 --clear-tags",
       ]
     ) { values, runtime in
       guard let input = values.argument(0) else {
@@ -51,9 +66,25 @@ enum EditCommand {
         throw RemindCoreError.reminderNotFound(input)
       }
 
-      let title = values.option("title")
+      var title = values.option("title")
       let listName = values.option("list")
       let notes = values.option("notes")
+      let addTags = try CommandHelpers.parseTags(values.optionValues("tag"))
+      let removeTags = try CommandHelpers.parseTags(values.optionValues("removeTag"))
+      let clearTags = values.flag("clearTags")
+      let hasTagChange = !addTags.isEmpty || !removeTags.isEmpty || clearTags
+
+      if hasTagChange {
+        let sourceTitle = title ?? reminder.title
+        let parsed = CommandHelpers.parseTitleTags(sourceTitle)
+        let merged = CommandHelpers.mergeTags(
+          existing: parsed.tags,
+          add: addTags,
+          remove: removeTags,
+          clear: clearTags
+        )
+        title = CommandHelpers.composeTitle(baseTitle: parsed.baseTitle, tags: merged)
+      }
 
       var dueUpdate: Date??
       if let dueValue = values.option("due") {
@@ -78,7 +109,9 @@ enum EditCommand {
       }
       let isCompleted: Bool? = completeFlag ? true : (incompleteFlag ? false : nil)
 
-      if title == nil && listName == nil && notes == nil && dueUpdate == nil && priority == nil && isCompleted == nil {
+      if title == nil && listName == nil && notes == nil && dueUpdate == nil && priority == nil
+        && isCompleted == nil && !hasTagChange
+      {
         throw RemindCoreError.operationFailed("No changes specified")
       }
 

Sources/reminders/Commands/ShowCommand.swift
@@ -23,7 +23,13 @@ enum ShowCommand {
               names: [.short("l"), .long("list")],
               help: "Limit to a specific list",
               parsing: .singleValue
-            )
+            ),
+            .make(
+              label: "tag",
+              names: [.long("tag")],
+              help: "Filter by tag (repeatable or comma-separated)",
+              parsing: .singleValue
+            ),
           ]
         )
       ),
@@ -33,10 +39,12 @@ enum ShowCommand {
         "reminders show overdue",
         "reminders show 2026-01-04",
         "reminders show --list Work",
+        "reminders show --tag shopping",
       ]
     ) { values, runtime in
       let listName = values.option("list")
       let filterToken = values.argument(0)
+      let tagFilters = try CommandHelpers.parseTags(values.optionValues("tag")).map { $0.lowercased() }
 
       let filter: ReminderFilter
       if let token = filterToken {
@@ -51,7 +59,17 @@ enum ShowCommand {
       let store = RemindersStore()
       try await store.requestAccess()
       let reminders = try await store.reminders(in: listName)
-      let filtered = ReminderFiltering.apply(reminders, filter: filter)
+      let filteredByDate = ReminderFiltering.apply(reminders, filter: filter)
+      let filtered: [ReminderItem]
+      if tagFilters.isEmpty {
+        filtered = filteredByDate
+      } else {
+        let tagFilterSet = Set(tagFilters)
+        filtered = filteredByDate.filter { reminder in
+          let reminderTags = Set(reminder.tags.map { $0.lowercased() })
+          return !tagFilterSet.isDisjoint(with: reminderTags)
+        }
+      }
       OutputRenderer.printReminders(filtered, format: runtime.outputFormat)
     }
   }